0024ed0781a5b0e9b58871c5bbbfd8e424b86b35
Ma-murakami <166308764+Ma-murakami@users.noreply.github.com>
2024-06-01 20:02:06 +0900

    Update step3-1_scraping.py

--- step3-1_scraping.py
+++ step3-1_scraping.py
@@ -14,7 +14,8 @@ def get_html(url):
 # 東京23区を対象とした検索URL
 base_url = "https://suumo.jp/jj/chintai/ichiran/FR301FC001/?ar=030&bs=040&ta=13&sc=13101&sc=13102&sc=13103&sc=13104&sc=13105&sc=13113&sc=13106&sc=13107&sc=13108&sc=13118&sc=13121&sc=13122&sc=13123&sc=13109&sc=13110&sc=13111&sc=13112&sc=13114&sc=13115&sc=13120&sc=13116&sc=13117&sc=13119&cb=0.0&ct=9999999&mb=0&mt=9999999&et=9999999&cn=9999999&shkr1=03&shkr2=03&shkr3=03&shkr4=03&sngz=&po1=25&pc=50&page={}"
 all_data = []
-max_page = 2195
+max_page = 1
+#テスト用(2195が元の値）
 
 for page in range(1, max_page+1):
     # URLを定義
